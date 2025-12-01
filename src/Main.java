d019654116e8facc08b716cc539ab0f2035849cb
jalaverdiyeva <93456693+jalaverdiyeva@users.noreply.github.com>
2025-12-01 14:23:29 +0400

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+//while
+
 import java.util.Scanner;
 
 public class Main{
@@ -37,6 +40,48 @@ public class Main{
             System.out.println("zəhmət olmasa bankla əlaqə saxlayın 😿");
         }
 
+        scanner.close();
+    }
+}
+
+//---------------------------------------------------------------------------------------------
+
+
+//for
+
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int duzgunPin=7532;//düzgün PİN kodu
+        int maksimumCehd=3;
+        boolean sistemeDaxilOldu=false;
+        System.out.println();
+        System.out.println("      ----> ATM PİN KODU SİSTEMİ <----");
+        System.out.println("        (sizin " + maksimumCehd + " cəhd haqqınız var)");
+        System.out.println();
+        
+        for(int cehd=1; cehd<=maksimumCehd && !sistemeDaxilOldu; cehd++){//for dövrü ilə həll
+            System.out.print("🔧PİN kodunuzu daxil edin: ");
+            int daxilEdilenPin = scanner.nextInt();
+            
+            if (daxilEdilenPin==duzgunPin) {
+                System.out.println("👏🏼 sistemə daxil oldunuz!");
+                sistemeDaxilOldu = true;
+                break; // Dövrü dayandır
+            } else {
+                if (cehd < maksimumCehd) {
+                    System.out.println("❗️️ yanlış PİN -> yenidən cəhd edin");
+                    System.out.println("         (qalan cəhd sayı: " + (maksimumCehd - cehd)+")");
+                    System.out.println();
+                }
+            }
+        }
+        //əgər 3 cəhd bitibsə və hələ də daxil olmayıbsa
+        if (!sistemeDaxilOldu) {
+            System.out.println("‼️ kartınız bloklandı!");
+            System.out.println("zəhmət olmasa bankla əlaqə saxlayın 😿");
+        }
         scanner.close();
     }
 }
